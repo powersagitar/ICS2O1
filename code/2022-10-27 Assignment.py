a8ad88b1450e3b542a085f68c30370e5ac342f78
powersagitar <powersagitar@users.noreply.github.com>
2022-10-27 17:37:50 -0400

    Revert "Update 2022-10-27 Assignment.py"
    This reverts commit 0651409a69f8de013fe011b98335d1d9ecf49f40.

--- code/2022-10-27 Assignment.py
+++ code/2022-10-27 Assignment.py
@@ -12,17 +12,12 @@ inMin = int(input("Enter your in time (Minute): "));
 outHour = int(input("Enter your out time (Hour): "));
 outMin = int(input("Enter your out time (Minute): "));
 
-if inHour > outHour:
+if inHour >= outHour:
     duration = ((23 - inHour) * 60) + (60 - inMin) + (outHour * 60) + outMin;
-elif inHour == outHour:
-    if inMin >= outMin: #the next day
-        duration = ((23 - inHour) * 60) + (60 - inMin) + (outHour * 60) + outMin;
-    else: #the same day
-        duration = outMin - inMin;
 else:
     duration = ((outHour - inHour) * 60) + (60 - inMin) + outMin;
     if inMin == 0:
-        duration -= 60;
+        duration -= 
 
 if (duration % 30) != 0:
     period = duration // 30 + 1;
