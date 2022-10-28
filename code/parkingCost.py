0dfcefc0f26867c5d49b4f593cd181ef1673b58f
powersagitar <114868133+powersagitar@users.noreply.github.com>
2022-10-28 14:53:32 -0400

    update parkingCost.py

--- code/parkingCost.py
+++ code/parkingCost.py
@@ -7,37 +7,44 @@
 
 #key point: the duration is not longer than 24 hours.
 #Line 19-30 will only work if the duration is not longer than 24 hours.
-while(True):
+#Subtotal will never has more than two decimal places.
+HST = 0.13
+CHARGE = 1.75
+inHour = int(input("Enter your in time (Hour): "));
+inMin = int(input("Enter your in time (Minute): "));
+outHour = int(input("Enter your out time (Hour): "));
+outMin = int(input("Enter your out time (Minute): "));
 
-    HST = 0.13
-    CHARGE = 1.75
-    inHour = int(input("Enter your in time (Hour): "));
-    inMin = int(input("Enter your in time (Minute): "));
-    outHour = int(input("Enter your out time (Hour): "));
-    outMin = int(input("Enter your out time (Minute): "));
-
-    if (inHour <= 23 and inMin <= 59 and outHour <= 23 and outMin <= 59):
-        if inHour > outHour: #the next day
+if (inHour <= 23 and inMin <= 59 and outHour <= 23 and outMin <= 59):
+    if inHour > outHour: #the next day
+        duration = ((23 - inHour) * 60) + (60 - inMin) + (outHour * 60) + outMin;
+    elif inHour == outHour:
+        if inMin >= outMin: #the next day
             duration = ((23 - inHour) * 60) + (60 - inMin) + (outHour * 60) + outMin;
-        elif inHour == outHour:
-            if inMin >= outMin: #the next day
-                duration = ((23 - inHour) * 60) + (60 - inMin) + (outHour * 60) + outMin;
-            else: #the same day
-                duration = outMin - inMin;
         else: #the same day
-            duration = ((outHour - inHour) * 60) + (60 - inMin) + outMin;
-            if inMin == 0:
-                duration -= 60;
+            duration = outMin - inMin;
+    else: #the same day
+        duration = ((outHour - inHour) * 60) + (60 - inMin) + outMin;
+        if inMin == 0:
+            duration -= 60;
 
-        if (duration % 30) != 0:
-            period = duration // 30 + 1;
-        else:
-            period = duration / 30;
-
-        print("duration Min:", duration);
-        print("duration Hrs:", duration / 60);
-        print("period:", period);
-        print();
+    if (duration % 30) != 0:
+        period = duration // 30 + 1;
     else:
-        print("Please enter a valid time.");
-        print();
+        period = duration // 30;
+
+    subtotal = CHARGE * period;
+    tax = round((subtotal * HST), 2);
+    
+    print("ICS2O1 Parking Management Inc.");
+    print("Your in time is:", str(inHour).zfill(2) + ":" + str(inMin).zfill(2));
+    print("Your out time is:", str(outHour).zfill(2) + ":" + str(outMin).zfill(2));
+    print("The duration of stay is:", duration, "minutes.");
+    print("Your staying periods are:", period, "periods.");
+    print("Subtotal: $" + str(subtotal));
+    print("HST: $" + str(tax));
+    print("Total: $" + str(subtotal + tax));
+    print();
+else:
+    print("Please enter a valid time.");
+    print();
