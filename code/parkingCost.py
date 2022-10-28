424ce3430564d8728e34314260ff916443e21b08
powersagitar <114868133+powersagitar@users.noreply.github.com>
2022-10-28 14:25:48 -0400

    Update parkingCost.py
    key point: the duration is not longer than 24 hours.
    Line 19-30 will only work if the duration is not longer than 24 hours.
    
    started debugging and testing, the structure is finished.

--- code/parkingCost.py
+++ code/parkingCost.py
@@ -5,6 +5,8 @@
 #Description: An assignment about parking cost.
 ##############################################################
 
+#key point: the duration is not longer than 24 hours.
+#Line 19-30 will only work if the duration is not longer than 24 hours.
 while(True):
 
     HST = 0.13
@@ -14,24 +16,28 @@ while(True):
     outHour = int(input("Enter your out time (Hour): "));
     outMin = int(input("Enter your out time (Minute): "));
 
-    if inHour > outHour: #the next day
-        duration = ((23 - inHour) * 60) + (60 - inMin) + (outHour * 60) + outMin;
-    elif inHour == outHour:
-        if inMin >= outMin: #the next day
+    if (inHour <= 23 and inMin <= 59 and outHour <= 23 and outMin <= 59):
+        if inHour > outHour: #the next day
             duration = ((23 - inHour) * 60) + (60 - inMin) + (outHour * 60) + outMin;
+        elif inHour == outHour:
+            if inMin >= outMin: #the next day
+                duration = ((23 - inHour) * 60) + (60 - inMin) + (outHour * 60) + outMin;
+            else: #the same day
+                duration = outMin - inMin;
         else: #the same day
-            duration = outMin - inMin;
-    else: #the same day
-        duration = ((outHour - inHour) * 60) + (60 - inMin) + outMin;
-        if inMin == 0:
-            duration -= 60;
+            duration = ((outHour - inHour) * 60) + (60 - inMin) + outMin;
+            if inMin == 0:
+                duration -= 60;
 
-    if (duration % 30) != 0:
-        period = duration // 30 + 1;
+        if (duration % 30) != 0:
+            period = duration // 30 + 1;
+        else:
+            period = duration / 30;
+
+        print("duration Min:", duration);
+        print("duration Hrs:", duration / 60);
+        print("period:", period);
+        print();
     else:
-        period = duration / 30;
-
-    print("duration Min:", duration);
-    print("duration Hrs:", duration / 60);
-    print("period:", period);
-    print();
+        print("Please enter a valid time.");
+        print();
