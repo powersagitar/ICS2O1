033ebafaeaddbda4475f659199507efdb682de80
fireflyTmp <117117710+fireflyTmp@users.noreply.github.com>
2022-11-01 15:04:55 -0400

    initial update

---
+++ src/nestedLoop/doc.md
@@ -0,0 +1,8 @@
+# nestedLoop.py
+## Data Structure
+* `DISCOUNT`
+  * Value: `0.9` (90%)
+  * Special day: `MM` = `DD`
+* `specificationStore`
+  * `void`: no specifications
+  * non-void: specifications
--- src/nestedLoop/main.py
+++ src/nestedLoop/main.py
@@ -1,1 +1,43 @@
+##############################################################
+# Program: Menu - Nested Loop
+# Date: 11/01/2022
+# Author: Mohan D.
+# Description: An assignment about menus.
+##############################################################
 
+# discount declaration
+dateM = int(input("Enter today's date (MM): "));
+dateD = int(input("Enter today's date (DD): "));
+if (dateM == dateD):
+    DISCOUNT = 0.9
+
+while (True):
+    name = input("Enter your full name: ");
+    specificationStore = [];
+    # fetch specifications
+    while (True):
+        specificationInput = input("Enter your specifications, if none, enter *void*: ");
+        specificationStore.append(specificationInput);
+        if ("void" in specificationStore):
+            specificationStore = "None";
+            break;
+        # new specification check
+        conditionInnerWhile1 = input("Continue? Enter True or False: ");
+        if (conditionInnerWhile1 == "False"):
+            break;
+    
+    
+
+    # printing receipt
+    print("\nReceipt");
+    print("Name:", name);
+    print("Specifications:", specificationStore);
+    if (DISCOUNT is not None):
+        print("Special-day discount applied:", DISCOUNT);
+    else:
+        print("No special-day discount applied");
+    
+    # new customer check
+    conditionOuterWhile = input("Is there any more customers? Enter True or False: ")
+    if (conditionOuterWhile == "False"):
+        break;
