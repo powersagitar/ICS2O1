3c5b619808e25d11a83e58cd5c18150f47c849d7
Firefly <117117710+fireflyTmp@users.noreply.github.com>
2022-11-15 13:55:18 -0500

    update algorithm lib

--- src/nestedLoop/lib.py
+++ src/nestedLoop/lib.py
@@ -1,10 +1,7 @@
 # find the nth occurrence of the divider
 def findnth(string, substring, n):
-    parts = string.split(substring, n + 1)
+    parts = string.split(substring, n)
     if len(parts) <= n + 1:
         return -1
     return len(string) - len(parts[-1]) - len(substring)
 findnth('foobarfobar akfjfoobar afskjdf foobar', 'foobar', 2)
-
-# thinking
-# n starts from 0 (eg. the first occurrence is 0)
