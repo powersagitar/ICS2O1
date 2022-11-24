481983c1d619ae53451eb4083068023482e3a69b
Firefly <117117710+fireflyTmp@users.noreply.github.com>
2022-11-23 23:16:25 -0500

    Add new features
    Added all basic elements of a house (some elements were missing in the initial update).

--- src/turtle/house.py
+++ src/turtle/house.py
@@ -3,12 +3,15 @@ import turtle
 import math
 
 # defining functions
-def drawSimple(polygon, length1, length2, penColor, fillColor, xStart, yStart):
-    turtle.setpos(xStart, yStart);
+# draw basic shapes
+def drawSimple(polygon, length1, length2, penColor, fillColor, position):
+    # initializing
+    turtle.setpos(position);
     angle = 360 / int(polygon);
     turtle.pencolor(penColor);
     turtle.fillcolor(fillColor);
 
+    # drawing
     turtle.pd();
     turtle.begin_fill();
     for i in range(math.ceil(polygon / 2)):
@@ -19,31 +22,97 @@ def drawSimple(polygon, length1, length2, penColor, fillColor, xStart, yStart):
     turtle.end_fill();
     turtle.pu();
 
-def drawIsosceles(length, angle, rotation, xStart, yStart):
-    turtle.setpos(xStart, yStart);
+# draw isosceles segments
+def drawIsosceles(length, angle, rotation, pencolor, position):
+    # initializing
+    turtle.setpos(position);
+    turtle.pencolor(pencolor);
+    
+    # drawing
     turtle.pd();
-
     for i in range(rotation):
         turtle.forward(length);
         turtle.right(angle);
     turtle.pu();
 
-# def main()
+# regular (non-preset) drawing
+def draw(length, heading, pencolor, position):
+    turtle.setpos(position); # initializing
+    
+    # drawing
+    turtle.seth(heading);
+    turtle.pencolor(pencolor);
+    turtle.pd();
+    turtle.forward(length);
+    turtle.pu();
+
+# draw segments which have specific starting and ending positions
+def drawSeg(starting, ending, pencolor):
+    # initializing
+    turtle.pencolor(pencolor);
+    turtle.setpos(starting);
+
+    # drawing
+    turtle.pd();
+    turtle.setpos(ending);
+    turtle.pu();
+
+# draw circles
+def drawCircle(color, radius, position):
+    turtle.pencolor(color);
+    turtle.fillcolor(color);
+    turtle.setpos(position);
+    turtle.begin_fill();
+    turtle.circle(radius);
+    turtle.end_fill();
+
+# def main():
+# initializing
 turtle.setup(1000, 1000); # setting up canvas
 turtle.pu();
 turtle.speed("fastest"); # code 0 <int>
 
-drawSimple(4, 1000, 400, "blue", "blue", -500, 500); # drawing a sky
+drawSimple(4, 1000, 400, "blue", "blue", (-500, 500)); # drawing a sky
+drawCircle("yellow", 100, (-450, 350)); # drawing a sun
+drawSimple(4, 1000, 600, "green", "green", (-500, 100)); # drawing the gound
+drawSimple(4, 600, 500, "grey", "grey", (-350, 200)); # drawing bottom rectangle
 
-# drawing a sun
-turtle.setpos(-450, 350)
-turtle.pencolor("yellow");
-turtle.fillcolor("yellow");
-turtle.begin_fill()
-turtle.circle(100); 
+# drawing the roof
+turtle.fillcolor("red");
+turtle.begin_fill();
+draw(250, 55, "red", (-350, 200));
+draw(600, 0, "red", turtle.pos());
+posTmp0 = turtle.pos();
+draw(250, 235, "red", turtle.pos());
 turtle.end_fill();
 
-drawSimple(4, 1000, 600, "green", "green", -500, 100); # drawing the gound
-drawSimple(4, 600, 500, "black", "black", -300, 200); # drawing rectangular part of the house
+# drawing top-right triangle
+turtle.fillcolor("yellow");
+turtle.begin_fill();
+draw(200, 10, "yellow", turtle.pos());
+posTmp1 = turtle.pos();
+drawSeg(posTmp1, posTmp0, "yellow"); 
+turtle.end_fill();
+
+# drawing right parallelogram
+turtle.fillcolor("brown");
+turtle.begin_fill();
+drawSeg(posTmp1, (446.96, -265.27), "brown");
+drawSeg(turtle.pos(), (250, -300), "brown");
+drawSeg(turtle.pos(), (250, 200), "brown");
+drawSeg(turtle.pos(), posTmp1, "brown");
+turtle.end_fill();
+
+# drawing windows
+turtle.seth(0);
+drawSimple(4, 150, 100, "blue", "blue", (-250, 100));
+turtle.width(5);
+draw(100, 270, "grey", (-175, 100));
+turtle.width(2.5);
+draw(150, 0, "grey", (-250, 50));
+
+# drawing doors
+drawSimple(4, 100, 400, "white", "white", (100, 100)); # door
+drawCircle("grey", 7, (170, 0)); # handle
 
 turtle.exitonclick();
