184c89c425744afdac6ed08103d2075d6869dde5
Firefly <117117710+fireflyTmp@users.noreply.github.com>
2022-11-25 14:07:48 -0500

    Add support for registering turtle shape
    Added line 8, 9, modified line 6 in order to make canvas fit on monitor.

--- src/turtle/11-24-2022/main.py
+++ src/turtle/11-24-2022/main.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 
 # def main():
-turtle.setup(1200, 1200);
+turtle.setup(1000, 1000);
 turtle.bgpic(Path("./include/bg.png"));
+turtle.addshape("./include/pen.gif");
+turtle.shape("./include/pen.gif");
 turtle.update();
 
 turtle.exitonclick();
