83ef8a4b96b7b8e24ec1d2d0969881f11bbdd44d
Firefly <117117710+fireflyTmp@users.noreply.github.com>
2022-11-25 14:47:48 -0500

    Add comments
    Added line-by-line comments for body.

--- src/turtle/11-24-2022/main.py
+++ src/turtle/11-24-2022/main.py
@@ -1,12 +1,13 @@
+# importing libraries
 import turtle
 from pathlib import Path
 
 
 # def main():
-turtle.setup(1000, 1000);
-turtle.bgpic(Path("./include/bg.png"));
-turtle.addshape("./include/pen.gif");
-turtle.shape("./include/pen.gif");
-turtle.update();
+turtle.setup(1000, 1000); # setting up the canvas
+turtle.bgpic(Path("./include/bg.png")); # setting background image
+turtle.addshape("./include/pen.gif"); # registering pen shape
+turtle.shape("./include/pen.gif"); # updating pen shape
+turtle.update(); # update canvas
 
 turtle.exitonclick();
