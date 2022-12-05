cefc1c7fd2e6b8f77d5eed001ffbac0b6f36c1e5
powersagitar <powersagitar@users.noreply.github.com>
2022-12-05 15:45:47 -0500

    update code efficiency optimized oval generation

--- src/turtle/12-05-2022/main.py
+++ src/turtle/12-05-2022/main.py
@@ -1,10 +1,16 @@
+############################################
+# Program: screensaver.py
+# Date: 12/05/2022
+# Author: Mohan Dong
+# Description: Oval screensaver.
+############################################
+
 from random import randint
 import turtle
 
 def initialize(color):
     turtle.title("Group 2");
     turtle.setup(1000, 1000);
-    turtle.pu;
     turtle.speed("fastest");
     turtle.clear();
     turtle.color(color);
@@ -13,19 +19,18 @@ def initialize(color):
     turtle.shapesize(8, 4, 1);
     turtle.end_fill();
 
-def oval(position, color):
-    turtle.pu();
-    turtle.setpos(position);
-    
-
 def main():
-    initialize();
+    # initialize
+    initialize("green");
     x = randint(200, 300); y = 240;
-    oval((x, y), "green");
+    turtle.pu();
+    turtle.setpos(x, y);
     xReverse = False; yReverse = False;
     xStep = 2; yStep = -3;
 
+    # bouncing
     while True:
+        # boucing eligibility check
         if x > -450 and x < 450:
             print("line 29")
             xReverse = False;
@@ -33,6 +38,7 @@ def main():
             print("line 32")
             yReverse = False;
 
+        # reverse direction
         if (x >= 500 or x <= -500) and xReverse == False:
             print("line 36")
             xStep = -xStep;
@@ -42,10 +48,11 @@ def main():
             yStep = -yStep;
             yReverse = True;
         
+        # update pen position
         x += xStep; y += yStep;
-        oval((x, y), "green");
+        turtle.setpos(x, y);
 
-        print("x:", x, "y:", y, "\nxStep:", xStep, "yStep:", yStep, "\nreverse status:", "x:", xReverse, "y:", yReverse);
+        print("x:", x, "y:", y, "\nxStep:", xStep, "yStep:", yStep, "\nreverse status:", "x:", xReverse, "y:", yReverse); # log
 
 
 if __name__ == '__main__':
