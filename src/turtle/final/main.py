1e2185347ab9300c6502405e247e988d15baebc1
Firefly <117117710+fireflyTmp@users.noreply.github.com>
2022-12-16 14:26:05 -0500

    update comments

--- src/turtle/final/main.py
+++ src/turtle/final/main.py
@@ -11,8 +11,7 @@ from math import ceil
 from random import randint
 from time import sleep
 
-# pen instantiation
-pen = turtle.Turtle();
+pen = turtle.Turtle(); # pen instantiation
 
 # color definition
 darkGreen = "#3f9523";
@@ -36,100 +35,105 @@ reservedPx = []; # storing reserved positions
 # draw basic shapes
 def drawSimple(polygon, length1, length2, pensize, pencolor, fillColor, position):
     # initializing
-    pen.setpos(position);
-    angle = 360 / int(polygon);
-    pen.pencolor(pencolor);
-    pen.fillcolor(fillColor);
-    pen.pensize(pensize);
+    pen.setpos(position); # set starting position
+    angle = 360 / int(polygon); # angle value for each corner
+    pen.pencolor(pencolor); # set pen color
+    pen.fillcolor(fillColor); # set fill color
+    pen.pensize(pensize); # set pen size
 
     # drawing
-    pen.pd();
-    pen.begin_fill();
+    pen.pd(); # drop the pen
+    pen.begin_fill(); # start filling
+
+    # body part || print the shape
     for i in range(ceil(polygon / 2)):
         pen.forward(length1);
         pen.right(angle);
         pen.forward(length2);
         pen.right(angle);
-    pen.end_fill();
-    pen.pu();
+
+    pen.end_fill(); # stop filling
+    pen.pu(); # lift the pen
 
 # draw segments which have specific starting and ending positions
 def drawSeg(starting, ending, pensize, pencolor):
     # initializing
-    pen.pencolor(pencolor);
-    pen.setpos(starting);
-    pen.pensize(pensize);
+    pen.pencolor(pencolor); # set pen color
+    pen.setpos(starting); # set first end point
+    pen.pensize(pensize); # set pen size
 
     # drawing
-    pen.pd();
-    pen.setpos(ending);
-    pen.pu();
+    pen.pd(); # drop the pen
+    pen.setpos(ending); # set second end point
+    pen.pu(); # lift the pen
 
 # draw a pixel 20 * 20
 def drawPx(position, color, pensize):
     # initializing
-    pen.pencolor(color);
-    pen.fillcolor(color);
-    pen.setpos(position);
-    pen.pensize(pensize);
+    pen.pencolor(color); # set pen color
+    pen.fillcolor(color); # set fill color
+    pen.setpos(position); # set starting position
+    pen.pensize(pensize); # set pensize
 
     # drawing
-    pen.pd();
-    pen.begin_fill();
+    pen.pd(); # drop the pen
+    pen.begin_fill(); # start filling
+
+    # body part || draw a pixel
     for i in range(4):
         pen.forward(20);
         pen.right(90);
-    pen.end_fill();
-    pen.pu();
+
+    pen.end_fill(); # stop filling
+    pen.pu(); # life the pen
 
 # extending reserved pixels
 def extendReservedPx(xInit, yInit, xStep, yStep, limit):
-    for i in range(limit):
-        if not [xInit, yInit] in reservedPx:
-            reservedPx.append([xInit, yInit]);
-        xInit += xStep; yInit += yStep;
+    for i in range(limit): # set iteration counter
+        if not [xInit, yInit] in reservedPx: # data duplication check
+            reservedPx.append([xInit, yInit]); # append new data
+        xInit += xStep; yInit += yStep; # move to next target pixel
 
 # draw the grid
 def grid(color):
-# horizontal
-    vertical = -500; horizontal = 500;
-    for i in range(50):
-        drawSeg((-500, horizontal), (500, horizontal), 1, color);
-        horizontal -= 20;
+    vertical = -500; horizontal = 500; # set initial value
 
-    # vertical
-    for i in range(50):
-        drawSeg((vertical, 500), (vertical, -500), 1, color);
-        vertical += 20;
+    for i in range(50): # set iteration counter
+        drawSeg((-500, horizontal), (500, horizontal), 1, color); # draw a horizontal line
+        drawSeg((vertical, 500), (vertical, -500), 1, color); # draw a vertical line
+        horizontal -= 20; vertical += 20; # move to next target lines
 
 # initializing
 def initialize():
-    turtle.hideturtle();
-    turtle.setup(1000, 1000);
-    pen.pu();
-    pen.speed("fastest");
+    turtle.hideturtle(); # hide turtle
+    turtle.setup(1000, 1000); # set up canvas
+    pen.pu(); # lift the pen
+    pen.speed("fastest"); # set pen speed to fastest
 
     # starting text || title
+    # happy
     for i in range(8, 75, 3):
-        pen.write("HAPPY", False, "center", ("Rage", i, "italic"));
-        sleep(0.0625);
-        pen.clear();
+        pen.write("HAPPY", False, "center", ("Rage", i, "italic")); # print text
+        sleep(0.0625); # delay for vision persistence
+        pen.clear(); # clear screen for next output
+
+    # holidays
     for i in range(75, 120, 2):
-        pen.write("HOLIDAYS", False, "center", ("Rage", i, "italic"));
-        sleep(0.0625);
-        pen.clear();
+        pen.write("HOLIDAYS", False, "center", ("Rage", i, "italic")); # print text
+        sleep(0.0625); # delay for vision persistence
+        pen.clear(); # clear screen for next output
 
     # background
-    # var
+    # var definition
     start = -500; end = 500;
     pencolor = [mediumRed, darkGreen, mediumRed, white];
 
     # red, green, white stripes
     for i in range(100):
-        drawSeg((start, 500), (-500, end), 20, pencolor[i % 4]);
-        start += 20; end -= 20;
+        drawSeg((start, 500), (-500, end), 20, pencolor[i % 4]); # draw colored stripes
+        start += 20; end -= 20; # navigate to next target line
 
-    drawSimple(4, 920, 920, 5, black, white, (-462.5, 462.5)); # white square
+    drawSimple(4, 920, 920, 5, black, white, (-462.5, 462.5)); # white square || white background
 
     grid(grey); # grid
 
@@ -137,38 +141,38 @@ def initialize():
 # cross
 def cross(x, y, color):
     drawPx((x, y), color, 1); # center
-    drawPx((x, y + 20), color, 1); # upper
-    drawPx((x, y - 20), color, 1); # lower
+    drawPx((x, y + 20), color, 1); # top
+    drawPx((x, y - 20), color, 1); # bottom
     drawPx((x - 20, y), color, 1); # left
     drawPx((x + 20, y), color, 1); # right
 
-    # making sure no overlapping will occur
+    # making sure no overlapping will occur || traverse through all pixels in a 5px by 5px space
     for resX in range(x - 40, x + 41, 20):
         for resY in range(y - 40, y + 41, 20):
             reservedPx.append([(resX, resY)]);
 
-# candyCane
+# candy cane
 def candyCane():
     # blocks on lefter left
-    y = -400;
+    y = -400; # var definition
     for i in range(7):
-        drawPx((-420, y), mediumRed, 1);
-        y += 40;
+        drawPx((-420, y), mediumRed, 1); # draw and color the pixel
+        y += 40; # navigate to next target pixel
     
     # blocks on righter left
-    y = -380;
+    y = -380; # var definition
     for i in range(7):
-        drawPx((-400, y), mediumRed, 1);
-        y += 40;
+        drawPx((-400, y), mediumRed, 1); # draw and color the pixel
+        y += 40; # navigate to next target pixel
     
     # blocks on top
-    x = -360;
+    x = -360; # var definition
     for i in range(2):
-        drawPx((x, -140), mediumRed, 1);
-        x += 40;
+        drawPx((x, -140), mediumRed, 1); # draw and color the pixel
+        x += 40; # navigate to next target pixel
     
     # blocks on right
-    drawPx((-320, -180), mediumRed, 1);
+    drawPx((-320, -180), mediumRed, 1); # draw and color the pixels
 
     # border
     drawSeg((-420, -420), (-420, -160), 3, black); # lefter left
@@ -182,94 +186,99 @@ def candyCane():
     drawSeg((-320, -200), (-300, -200), 3, black); # lower right
     drawSeg((-300, -140), (-300, -200), 3, black); # righter right
 
-    # extending reserved pixels
+    # extending reserved pixels || traverse through the rectangular area where the candy cane is
     for i in range(-460, -280, 20):
         extendReservedPx(i, -460, 0, 20, 18);
 
 # christmas tree
 def christmasTree():
     # leaves
+    # middle and right half
     top = 120;
+    # traverse all the pixels where the christmas tree will be
     for x in range(0, 181, 20):
         for y in range(top, -301, -20):
-            drawPx((x, y), darkGreen, 1);
-        top -= 60;
+            drawPx((x, y), darkGreen, 1); # draw and color the pixel
+        top -= 60; # nevigate to the next target pixel
+    
+    # left half
     top = 60;
+    # traverse all the pixels where the christmas tree will be
     for x in range(-20, -181, -20):
         for y in range(top, -301, -20):
-            drawPx((x, y), darkGreen, 1);
-        top -= 60;
+            drawPx((x, y), darkGreen, 1); # draw and color the pixel
+        top -= 60; # nevigate to the next target pixel
     
     # trunk
+    # traverse all the pixels where the trunk will be
     for i in range(-320, -421, -20):
-        drawPx((0, i), brown, 1);
+        drawPx((0, i), brown, 1); # draw and color the trunk
 
-    # extending reserved pixels
+    # extending reserved pixels || traverse a rectangular area where the christmas tree is
     for i in range(-160, 161, 20):
         extendReservedPx(i, 140, 0, -20, 30);
     
-    cross(0, 120, yellow); # adding the star
+    cross(0, 120, yellow); # adding the yellow star on top
 
 def gift(x, y, topColor, bottomColor):
-    extendReservedPx(x - 20, y - 40, 20, 0, 6); # extend reserved pixels
+    extendReservedPx(x - 20, y - 40, 20, 0, 6); # extend reserved pixels || traverse through a rectangular area where the gifts are
 
     # cover
     for i in range(2):
         for i in range(x, x + 101, 20):
             drawPx((i, y), topColor, 1);
-        y -= 20;
+        y -= 20; # navigate to next target pixel
 
     # body
     for i in range(2):
         for i in range(x + 20, x + 81, 20):
             drawPx((i, y), bottomColor, 1);
-        y -= 20;
+        y -= 20; # navigate to next target pixel
 
     # belt
     y = -340;
     for i in range(2):
         for i in range(x + 40, x + 61, 20):
             drawPx((i, y), yellow, 1);
-        y -= 20;
+        y -= 20; # navigate to next target pixel
 
 def main():
     print("[program starting]");
-    # initializing
-    initialize();
+    initialize(); # initialize canvas
     print("[initialized]");
 
     # elements
     print("[building candy cane]");
-    candyCane(); # candyCane
+    candyCane(); # candy cane
     print("[done]");
     
-    # christmas tree
     print("[building christmas tree]");
-    christmasTree();
+    christmasTree(); # christmas tree
     print("[done]");
     
     # gifts under the christmas tree
     print("[building gifts under christmas tree]");
-    gift(-120, -360, darkGreen, mediumRed);
-    gift(20, -360, mediumRed, darkGreen);
+    gift(-120, -360, darkGreen, mediumRed); # left side
+    gift(20, -360, mediumRed, darkGreen); # right side
     print("[done]");
 
     # snow
     print("[building snowflakes]");
-    i = 0
+    i = 0 # define iteration var
     while i < 20:
-        x = randint(-440, 380) // 20 * 20; y = randint(-400, 360) // 20 * 20;
-        if not [x, y] in reservedPx:
-            cross(x, y, aqua);
-            i += 1;
+        x = randint(-440, 380) // 20 * 20; y = randint(-400, 360) // 20 * 20; # make sure generated pixels are on the grid
+        if not [x, y] in reservedPx: # overlapping check
+            cross(x, y, aqua); # draw a snowflake
+            i += 1; # update iteration var
     print("[done]");
     
     # christmas tree lighting
     print("[building lighting for christmas tree]");
-    pen.hideturtle();
-    color = [yellow, pink, blue, mediumRed, brown, lightGreen, darkRed, lightRed, aqua, white, grey];
+    pen.hideturtle(); # hide pen
+    color = [yellow, pink, blue, mediumRed, brown, lightGreen, darkRed, lightRed, aqua, white, grey]; # define available lighting colors
     while True:
-        userInput = input("Do you want the lighting color be randomly generated? [y/n]");
+        userInput = input("Do you want the lighting color be randomly generated? [y/n]"); # mode selector
+        # random snow
         if (userInput == 'y'):
             print("[done]");
             while True:
@@ -279,6 +288,8 @@ def main():
                 drawPx((0, -160), color[randint(0, len(color) - 1)], 1);
                 drawPx((-40, -240), color[randint(0, len(color) - 1)], 1);
                 drawPx((60, -280), color[randint(0, len(color) - 1)], 1);
+        
+        # manual snow
         elif (userInput == 'n'):
             print("available color list:", color, "\nenter the index: ");
             drawPx((0, 0), color[int(input())], 1);
@@ -288,6 +299,8 @@ def main():
             drawPx((-40, -240), color[int(input())], 1);
             drawPx((60, -280), color[int(input())], 1);
             print("[done]");
+
+        # undefined input
         else:
             print("invalid input");
     
