8f9d9c9f7baa8342385293a22ae3b412e0ce13a8
Firefly <117117710+fireflyTmp@users.noreply.github.com>
2023-01-09 14:44:08 -0500

    initial update

--- src/turtle/final/main.py
+++ src/turtle/final/main.py
@@ -6,18 +6,22 @@
 ############################################
 
 import turtle
-from pathlib import Path
 
 def main():
     # instantiation
     catcher = turtle.Turtle()
+    falling = turtle.Turtle()
 
     # initialization
     turtle.setup(1000, 1000)
-    turtle.bgpic(Path("./assets/images/background.gif"))
-    turtle.register_shape("catcher", Path("./assets/images/catcher.gif"))
-    catcher.shape(catcher)
+    turtle.bgpic("./assets/images/background.gif")
+    turtle.register_shape("./assets/images/catcher.gif")
+    turtle.register_shape("./assets/images/falling.gif")
+    catcher.shape("./assets/images/catcher.gif")
     catcher.setpos(0, 0)
+    falling.shape("./assets/images/falling.gif")
+    falling.setpos(0, 0)
+
 
 if __name__ == '__main__':
     main()
